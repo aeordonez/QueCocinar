bdaebc09138ecfdb1105d17b2ef38f29b4ef9f88
Alejandro Ordoñez <167221570+aeordonez@users.noreply.github.com>
2024-06-02 22:53:51 -0300

    Update contacto.js
    Amplié el rango de números para el teléfono de 6-9 a 6-11.

--- js/contacto.js
+++ js/contacto.js
@@ -11,7 +11,7 @@ const expresiones = {
     nombre: /^[a-zA-ZÀ-ÿ\s]{2,20}$/,
     apellido: /^[a-zA-ZÀ-ÿ\s]{2,20}$/,
     correo: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
-    telefono: /^[09][0-9]{6,9}$/
+    telefono: /^[09][0-9]{6,11}$/
 }
 
 const campos = {
